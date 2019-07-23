29b3a3d0029880d3c9c1a49752a2b6862c7064ee
Leiver Andres Campeón <leiverandres04p@hotmail.com>
2019-07-23 11:55:02 -0500

    Update entry point function

--- run.py
+++ run.py
@@ -1,24 +1,32 @@
-from config import cfg
 import torch
 import argparse
 
+from blender.render_utils import Renderer, YCBRenderer
+from config import cfg
+from fuse.fuse import run
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--type', action='store', dest='type', type=str)
+parser.add_argument("--type", choices=["rendering", "fuse"])
+parser.add_argument("--model_type", type=str, default="cat")
 args = parser.parse_args()
 
 
 def run_rendering():
-    from blender.render_utils import Renderer, YCBRenderer
     # YCBRenderer.multi_thread_render()
     # renderer = YCBRenderer('037_scissors')
-    renderer=Renderer('cat')
+    renderer = Renderer(args.model_type)
     renderer.run()
 
 
 def run_fuse():
-    from fuse.fuse import run
     run()
 
 
-if __name__ == '__main__':
-    globals()['run_' + args.type]()
+if __name__ == "__main__":
+    globals()["run_" + args.type]()
+    if args.type == "rendering":
+        run_rendering()
+    elif args.type == "fuse":
+        run_rendering()
+    else:
+        raise ValueError("Rendering option not implemented")
